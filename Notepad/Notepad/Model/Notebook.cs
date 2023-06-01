ac74f15b6f71b73cb89932a80633c2c3d6987fa8
5Z4RY <116660667+5Z4RY@users.noreply.github.com>
2023-06-01 08:45:31 +0200

    Add files via upload

---
+++ Notepad/Notepad/Model/Notebook.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notepad.Model
+{
+    public class Notebook
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public string Name { get; set; }
+    }
+}
